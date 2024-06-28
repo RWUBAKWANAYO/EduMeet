3d6196fb43fbd697ed26769902de31355b5f399c
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-06-28 13:38:47 +0200

    Chore: setup db, peer & socket, create error handlers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,36 @@
 import app from "./app";
 import http from "http";
 import "dotenv/config";
+import { Server } from "socket.io";
+import { ExpressPeerServer } from "peer";
+import connectMongoDB from "./config/mongodb";
+import { globalErrorHandler } from "./middlewares";
+import { ErrorFormat } from "./utils";
+import { NextFunction, Request, Response } from "express";
 
 const server = http.createServer(app);
 
+connectMongoDB(process.env.MONGODB_URL || "");
+
+const peerServer = ExpressPeerServer(server, { path: "/" });
+
+app.use("/peerjs", peerServer);
+
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+});
 const PORT = process.env.PORT || 8080;
+
+app.use("*", (_req: Request, _res: Response, next: NextFunction) => {
+  const err = new ErrorFormat(`Can't find ${_req.originalUrl} on this server!`, 404);
+  next(err);
+});
+
+app.use(globalErrorHandler);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ server/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { ErrorFormat } from "./errorFormat";
